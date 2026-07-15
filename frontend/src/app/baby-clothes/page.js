fa34fd7f220dd011f13f62a6a395191d14117d3a
safetyfirstsolarLLC <safetyfirstsolar@gmail.com>
2026-07-15 13:12:49 -0500

    Create page.js

---
+++ frontend/src/app/baby-clothes/page.js
@@ -0,0 +1,8 @@
+export default function BabyClothesPage() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+      <h1>Boutique Baby Clothes</h1>
+      <p>Welcome to our premium baby clothing collection.</p>
+    </div>
+  );
+}
