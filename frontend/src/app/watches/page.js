0d99de85418b4b6a94d90ccddebdc72930b562e8
safetyfirstsolarLLC <safetyfirstsolar@gmail.com>
2026-07-15 13:15:47 -0500

    Create page.js

---
+++ frontend/src/app/watches/page.js
@@ -0,0 +1,8 @@
+export default function WatchesPage() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif' }}>
+      <h1>Luxury Watches</h1>
+      <p>Explore our curated collection of high-end timepieces.</p>
+    </div>
+  );
+}
